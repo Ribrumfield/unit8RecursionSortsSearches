2c3eabd27818e47b321ea3a54905a906d40bb9dd
Ribrumfield <RIBrumfield@student.naperville203.org>
2016-03-24 10:28:58 -0500

    finished word count

--- W C/WC.java
+++ W C/WC.java
@@ -1,15 +1,31 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 
 public class WC
 {
     public static void main(String [] args)
     {
-        File inputeFile = new File ("input.txt");
-        Scanner in = new Scanner ( inputFile );
-        
-        while (in.hasNextDouble())
+        File inputFile = new File ("input.txt");
+        int count = 0;
+        try
         {
-            double value = in.nextDouble();
+            Scanner in = new Scanner (inputFile );   
+            in.useDelimiter("[^A-Za-z]+");
+            while (in.hasNext())
+            {
+                String i = in.next();
+                System.out.println(i);
+                count ++;
+            }
+            in.close();
         }
+        catch(FileNotFoundException ae)
+        {
+            System.out.println("this file does not exsist");
+        }
+        
+        System.out.println("the total number of characters is " + count);
     }
 }
